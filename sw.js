70950737a21ea096c7e8c675bd1dd34fc7494499
HangYuay <wcharoenchaipaiboon@gmail.com>
2026-09-13 14:59:01 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // TCAS70 PS Student Advisor — Service Worker
 // Enables offline usage and PWA installation
 
-const CACHE_NAME = 'tcas70-ps-v95';
+const CACHE_NAME = 'tcas70-ps-v96';
 const ASSETS = [
   './',
   './index.html',
